df3ab2aba6c6885fd9ee8141be52d67bf7d07906
damdadi93 <77727581+damdadi93@users.noreply.github.com>
2023-09-18 17:53:06 +0900

    230918_팩맨만들기 강의
    유니티 짱을 이용한 팩맨 강의 백업

---
+++ Day4_Lecture/Assets/1.Scripts/ScenChanger.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ScenChanger : MonoBehaviour
+{
+    public string nextSceneName;
+    public bool gameWin;
+    public bool gameLose;
+    public Animator anim;
+
+
+   
+    void Start()
+    {
+        anim.SetBool("Win", gameWin);
+        anim.SetBool("Lose", gameLose);
+    }
+
+    
+    void Update()
+    {
+        if(Input.GetButtonDown("Submit"))
+            SceneManager.LoadScene(nextSceneName);
+
+        
+    }
+}
